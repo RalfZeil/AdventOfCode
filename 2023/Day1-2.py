144de648118fe4e3243dba61021edd581835bdf0
Ralf Zeilstra <ralfzeils@gmail.com>
2023-12-01 15:26:15 +0100

    Removed bloat

--- 2023/Day1-2.py
+++ 2023/Day1-2.py
@@ -3,46 +3,38 @@ from word2number import w2n
 
 numbersInText = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
+lines = []
+with open('inputs/input1-1.txt') as f:
+    lines = f.readlines()
 
-def main():
-    # Read out the file and put them into the lines array
-    lines = []
-    with open('inputs/input1-1.txt') as f:
-        lines = f.readlines()
+sumCalibration = 0
 
-    sumCalibration = 0
+for line in lines:
+    firstNumber: int = None
+    lastNumber: int = None
+    textNumbers: List[str] = []
 
-    for line in lines:
-        firstNumber: int = None
-        lastNumber: int = None
-        textNumbers: List[str] = []
+    # Extract written numbers from line
+    for num in numbersInText:
+        if num in line:
+            textNumbers.append(num)
 
-        # Extract written numbers from line
-        for num in numbersInText:
-            if num in line:
-                textNumbers.append(num)
+    # Extract numbers from line
+    for char in line:
+        if char.isdigit():
+            if firstNumber is None:
+                firstNumber = char
 
-        # Extract numbers from line
-        for char in line:
-            if char.isdigit():
-                if firstNumber is None:
-                    firstNumber = char
+            lastNumber = char
 
-                lastNumber = char
+    # Compare position of written and literal numbers in line
+    for textNum in textNumbers:
+        if line.find(textNum) < line.find(firstNumber):
+            firstNumber = textNum
 
-        # Compare position of written and literal numbers in line
-        for textNum in textNumbers:
-            if line.find(textNum) < line.find(firstNumber):
-                firstNumber = textNum
+        if line.rfind(textNum) > line.rfind(lastNumber):
+            lastNumber = textNum
 
-            if line.rfind(textNum) > line.rfind(lastNumber):
-                lastNumber = textNum
+    sumCalibration += int(str(w2n.word_to_num(firstNumber)) + str(w2n.word_to_num(lastNumber)))
 
-        print(firstNumber, lastNumber)
-        sumCalibration += int(str(w2n.word_to_num(firstNumber)) + str(w2n.word_to_num(lastNumber)))
-
-    print(sumCalibration)
-
-
-if __name__ == "__main__":
-    main()
+print(sumCalibration)
